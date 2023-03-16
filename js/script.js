2ea2f30544aa4c92afe5e99609bf6eb0eb55cea4
Likulka <Ange-l-ika98@mail.ru>
2023-03-17 00:40:46 +0400

    init commit

---
+++ js/script.js
@@ -0,0 +1,53 @@
+'use strict'
+import { Octokit, App } from "https://cdn.skypack.dev/octokit";
+
+const form = document.querySelector('form');
+const results = document.querySelector('.results');
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    form.repo.value ? searchRepo(form.repo.value) : showError(form) ;
+})
+
+
+
+function showError(form){
+  let error = document.createElement('p');
+  error.className= 'error';
+  error.innerHTML = 'Поле должно быть заполнено';
+  form.append(error);
+  form.repo.addEventListener('input', () => {error.remove()}, {once: true})
+}
+
+async function searchRepo(str){
+    const octokit = new Octokit({
+      auth: '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'
+    })
+
+    let resp = await octokit.request(`GET /search/repositories?q=${str}&per_page=10`, {
+      headers: {},
+    })
+
+    showResults(resp)
+}
+
+function showResults(resp){
+  results.innerHTML = '';
+  if (resp.data.items.length == 0) {
+    results.innerHTML='<h2>Ничего не найдено</h2>';
+    return;
+  }
+  resp.data.items.forEach(element => {
+      let date = new Date(element.created_at);
+      results.innerHTML += `<div class='results__item'><a target='_blank' href='${element.html_url}' class="result__name">${element.name}</a>
+        <p class="result__desc">${element.description}<p>  
+        <p class="result__lang"><span>Язык: </span>${element.language}</p>
+        <p class="result__watchers"><span>Просматривают: </span>${element.watchers}</p>
+        <p class="result__watchers"><span>Создан: </span>${date.toLocaleString()}</p></div>`;
+  });
+
+}
+
+
+
