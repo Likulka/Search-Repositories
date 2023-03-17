0e80d578301ba451565480131876ac7d49757f69
Likulka <Ange-l-ika98@mail.ru>
2023-03-17 13:09:42 +0400

    change requests code

--- js/script.js
+++ js/script.js
@@ -4,8 +4,6 @@ import { Octokit, App } from "https://cdn.skypack.dev/octokit";
 const form = document.querySelector('form');
 const results = document.querySelector('.results');
 
-console.log('edjasb')
-
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     form.repo.value ? searchRepo(form.repo.value) : showError(form) ;
@@ -22,24 +20,18 @@ function showError(form){
 }
 
 async function searchRepo(str){
-    const octokit = new Octokit({
-      auth: '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'
-    })
-
-    let resp = await octokit.request(`GET /search/repositories?q=${str}&per_page=10`, {
-      headers: {},
-    })
-
-    showResults(resp)
+    let obj = await fetch(`https://api.github.com/search/repositories?q=${str}&per_page=10`);
+    let result = await obj.json();
+    showResults(result);
 }
 
 function showResults(resp){
   results.innerHTML = '';
-  if (resp.data.items.length == 0) {
+  if (resp.items.length == 0) {
     results.innerHTML='<h2>Ничего не найдено</h2>';
     return;
   }
-  resp.data.items.forEach(element => {
+  resp.items.forEach(element => {
       let date = new Date(element.created_at);
       results.innerHTML += `<div class='results__item'><a target='_blank' href='${element.html_url}' class="result__name">${element.name}</a>
         <p class="result__desc">${element.description}<p>  
@@ -47,7 +39,6 @@ function showResults(resp){
         <p class="result__watchers"><span>Просматривают: </span>${element.watchers}</p>
         <p class="result__watchers"><span>Создан: </span>${date.toLocaleString()}</p></div>`;
   });
-
 }
 
 
